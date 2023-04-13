9a9854c47c8385a1d3a4d6a5031574ac9fd0834e
Ibrahim Abdulhakeem <ibrahimoloyin84@gmail.com>
2023-04-13 12:24:09 +0300

    Day 1 to 5 of 30 days of python

---
+++ class.py
@@ -0,0 +1,8 @@
+symptom_1 = input("what is your symptom?")
+symptom_2 = input("what is your symptom?")
+symptom_3 = input("what is your symptom?")
+print(symptom_1)
+print(symptom_2)
+print(symptom_3)
+
+
